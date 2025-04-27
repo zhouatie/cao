1d90bd31769c6f737c02ae030c4808d5fbd85513
zhouatie <zhouatie@gmail.com>
2025-04-27 20:48:46 +0800

    fix: Support for low versions of python

---
+++ src/zhouatie_cao/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+__main__ entry point for the zhouatie_cao package.
+This allows running the package as a module with 'python -m zhouatie_cao'
+"""
+
+import sys
+from zhouatie_cao.main import main
+
+if __name__ == "__main__":
+    sys.exit(main())
